73e3b2a90d3595229cd0291b6845b948282ab708
MwahMallah <maximus27rus@gmail.com>
2024-11-26 22:58:32 +0100

    Added utils module with conversion of degrees to radians.

---
+++ Utils.cpp
@@ -0,0 +1,7 @@
+#include "Utils.hpp"
+
+#include "math.h"
+
+double DegreesToRadians(double degrees) {
+    return degrees * (M_PI / 180);
+}
---
+++ Utils.hpp
@@ -0,0 +1,6 @@
+#ifndef __UTILS_HPP
+#define __UTILS_HPP
+
+double DegreesToRadians(double degrees);
+
+#endif
