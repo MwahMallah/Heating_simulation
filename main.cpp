c5a76ebecf3e4ef038bcb69938d91562586ddc95
MwahMallah <maximus27rus@gmail.com>
2024-11-28 17:52:09 +0100

    Added total statistic for collector's efficiency.

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ using std::cout;
 using std::endl;
 
 void parse_arguments(int& start_day, int& timespan, double& start_hour, double& lat, double& envAvgTemp, double& envTempDifference,
-    double& azimuth, double& altitude, double& c_incl, double& c_area, double& c_heatTransfer, double& c_AbsorpTransm,
+    double& azimuth, double& altitude, double& c_incl, double& c_area, double& c_transEff, double& c_heatTransfer, double& c_AbsorpTransm,
     int argc, char** argv) {
     for (int i = 1; i < argc; ++i) {
         if ((strcmp(argv[i], "--timespan") == 0 || (strcmp(argv[i], "--t") == 0)) && (i + 1 < argc)) {
@@ -31,6 +31,8 @@ void parse_arguments(int& start_day, int& timespan, double& start_hour, double& 
             altitude = std::stod(argv[++i]);
         } else if ((strcmp(argv[i], "--collector_inclination") == 0 || (strcmp(argv[i], "--col_i") == 0)) && (i + 1 < argc)) { 
             c_incl = std::stod(argv[++i]);
+        } else if ((strcmp(argv[i], "--transfer_efficiency") == 0 || (strcmp(argv[i], "--trans_eff") == 0)) && (i + 1 < argc)) { 
+            c_transEff = std::stod(argv[++i]);
         } else if ((strcmp(argv[i], "--heat_transfer") == 0 || (strcmp(argv[i], "--ht") == 0)) && (i + 1 < argc)) { 
             c_heatTransfer = std::stod(argv[++i]);
         } else if ((strcmp(argv[i], "--collector_absorptance") == 0 || (strcmp(argv[i], "--absorp") == 0)) && (i + 1 < argc)) { 
@@ -107,22 +109,63 @@ double get_useful_collector_energy(Collector& collector, Environment& env, int d
 }
 
 void simulate_power_balance_by_hour
-    (Collector& collector, Environment& env, House& house, int day, double latitude) {
-
+    (Collector& collector, Environment& env, House& house, int day, double latitude, double transferEfficiency) {
+    // Output file for storing power balance data (hour, collector energy, house energy loss)
     std::ofstream power_balance_by_hour("PlotData/power_balance_by_hour.dat");
-    
-    for (double hour = 0; hour <= 24; hour += 0.5) {
-        double collectorEnergy = get_useful_collector_energy(collector, env, day, latitude, hour);
-        double houseLossEnergy = house.GetHeatLoss(env.OutsideTemp(hour));
 
+    // Time slice for simulation (hours)
+    double time_slice = 0.5;
+
+    // Accumulators for total energy collected and energy lost
+    double total_energy_loss = 0;
+    double total_collected_energy = 0;
+
+    // Simulate energy balance for each time slice throughout the day
+    for (double hour = 0; hour <= 24; hour += time_slice) {
+        // Calculate power collected by the solar collector at the current hour
+        double current_collector_power = get_useful_collector_energy(collector, env, day, latitude, hour);
+
+        // Calculate heat loss from the house at the current hour
+        double current_house_loss_power = house.GetHeatLoss(env.OutsideTemp(hour));
+
+        // Debug output for monitoring simulation progress
         cout << "At time: " << hour << " hours" << endl;
-        cout << "Collector's energy: " << collectorEnergy  << " W" << endl;
-        cout << "House loss: " << houseLossEnergy << " W" << endl;
+        cout << "Collector's power: " << current_collector_power  << " W" << endl;
+        cout << "House loss: " << current_house_loss_power << " W" << endl;
         cout << endl;
 
-        //add new graph data
-        power_balance_by_hour << hour << " " << collectorEnergy << " " << houseLossEnergy << endl;
+        // Write the current hour's data to the file
+        power_balance_by_hour << hour << " " 
+                              << current_collector_power << " " 
+                              << current_house_loss_power << endl;
+
+        // Estimate average power values over the time slice for accurate integration
+        double avg_collector_power = get_useful_collector_energy(collector, env, day, latitude, hour + time_slice / 2);
+        double avg_house_loss_power = house.GetHeatLoss(env.OutsideTemp(hour + time_slice / 2));
+
+        // Accumulate energy values for the entire day
+        total_collected_energy += avg_collector_power * time_slice;
+        total_energy_loss += avg_house_loss_power * time_slice;
     }
+
+    // Calculate and display the ratio of collected energy to lost energy
+    double energy_ratio = total_collected_energy * transferEfficiency / total_energy_loss;
+
+    // Final results output
+    cout << "Simulation results:" << endl;
+    cout << "-------------------" << endl;
+    cout << "Total energy collected by the solar collector through the day: " 
+         << total_collected_energy << " J" << endl;
+    cout << "Total energy received by the house through the day (transfer efficiency " 
+         << transferEfficiency * 100 
+         << "%): " 
+         << total_collected_energy * transferEfficiency << " J" << endl;
+    cout << "Total energy lost by the house through the day: " 
+         << total_energy_loss << " J" << endl;
+    cout << "Ratio of received to lost energy: " 
+         << energy_ratio * 100 
+         << "%"
+         << endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -141,6 +184,7 @@ int main(int argc, char* argv[]) {
     double collectorAbsorptanceTransmittance = 0.8645;
     double collectorHeatLossCoefficient = 3.4;
     double collectorTemp = 40;
+    double collectorTransferEfficiency = 0.8;
 
     //default for Brno
     double latitude = 49.11;
@@ -151,7 +195,7 @@ int main(int argc, char* argv[]) {
 
     //parse command line arguments
     parse_arguments(start_day, timespan, start_hour, latitude, envAvgTemp, envTempDiff, azimuth, locationHeight, 
-    collectorInclination, collectorArea, collectorHeatTransferFactor, collectorAbsorptanceTransmittance, argc, argv);
+    collectorInclination, collectorArea, collectorTransferEfficiency, collectorHeatTransferFactor, collectorAbsorptanceTransmittance, argc, argv);
 
     //create objects for environment, collector and house
     Environment env(latitude, azimuth, locationHeight, envAvgTemp, envTempDiff);
@@ -167,7 +211,7 @@ int main(int argc, char* argv[]) {
     //set collector's inclination back to command line value
     collector.SetIncl(collectorInclination);
 
-    simulate_power_balance_by_hour(collector, env, house, start_day, latitude);
+    simulate_power_balance_by_hour(collector, env, house, start_day, latitude, collectorTransferEfficiency);
 
     return 0;
 }
